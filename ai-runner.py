40b58223cfb76127cbd93f525009e4de2887318d
Joshua L Rasmussen <josh.rasmussen@discorp.com>
2017-01-23 13:11:20 -0800

    Update AI scripts

---
+++ ai-runner.py
@@ -0,0 +1,93 @@
+import time
+import sys, getopt
+from random import randint
+from socketIO_client import SocketIO
+
+#
+# The expectation is that this class gets
+# overridden to implement the make_move method
+#
+class AIRunner:
+    # Takes in a 2d array indicating the state of the game
+    # returns an int that indicates which move they would like to make
+    def make_move(self, state):
+        return randint(0, len(state) - 1)
+
+def on_update_game_state(*args): 
+    print("Game Updated", args) 
+    if 'player_id' in args[0]:
+        runner.piece = args[0]['player_id']
+        runner.column_count = args[0]['cols']
+    runner.game_start = args[0]['started']
+
+    if runner.game_start:
+        current_turn = args[0]['currentTurn']
+        if current_turn == runner.piece:
+            # Delay to be able to watch the game
+            time.sleep(delay)
+                
+            move = runner.make_move(args[0]['state'])
+
+            # Implement a time limit that will send a forefit signal to the server
+            socketIO.emit('make_move', {'roomName' : 'ai-room', 'piece' : runner.piece, 'column' : move})
+
+def on_win(*args):
+    print("End of Game", args)
+    time.sleep(delay)
+    socketIO.emit('vote', {'roomName' : 'ai-room', 'vote' : 1})
+
+def on_error_message(*args):
+    print("ERROR :", args)
+
+def on_disconnected(*args):
+    print("Other player left")
+    sys.exit()
+
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hr:x:y:s:w:c:d:l:", ["room-name=", "cols=", "rows=", "screen-name=",
+            "to-win=", "create=", "delay=", "limit="])
+    except getopt.GetoptError:
+        print 'ai-runner.py --room-name=<room-name> --rows=<rows> --cols=<cols> --screen-name=<screen-name> --to-win=<to win> --create=<yes or no>'
+        sys.exit(2)
+
+    delay = 3
+    limit = 1
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'ai-runner.py --room-name=<room-name> --rows=<rows> --cols=<cols> --screen-name=<screen-name> --to-win=<to win> --create=<yes or no>'
+            sys.exit()
+        elif opt in("-r", "--room-name"):
+            room_name = arg
+        elif opt in("-x", "--cols"):
+            cols = int(arg)
+        elif opt in("-y", "--rows"):
+            rows = int(arg)
+        elif opt in("-s", "--screen-name"):
+            screen_name = arg
+        elif opt in("-w", "--to-win"):
+            to_win = int(arg)
+        elif opt in("-c", "--create"):
+            create = int(arg)
+        elif opt in("-d", "--delay"):
+            delay = float(arg)
+        elif opt in("-l", "--limit"):
+            limit = float(arg)
+
+    socketIO = SocketIO('localhost', 3000)
+    runner = AIRunner()
+    
+    socketIO.on('update_game_state', on_update_game_state)
+    socketIO.on('winner', on_win)
+    socketIO.on('error_message', on_error_message) 
+    socketIO.on('disconnect_message', on_disconnected)
+
+    if create == 1:
+        socketIO.emit('create_game', { 'roomName' : room_name , 'screenName' : screen_name,'rows' : rows, 'cols' : cols, 'toWin' : to_win })
+    else:
+        socketIO.emit('join_game', {'roomName' : room_name, 'screenName' : screen_name})
+    
+    try:
+        socketIO.wait()
+    except ConnectionError:
+        print e
