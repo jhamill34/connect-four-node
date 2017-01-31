9414caa06e8f29390d48ac02d62b125f830938dc
Joshua L Rasmussen <josh.rasmussen@discorp.com>
2017-01-31 11:51:13 -0800

    Remove the ai from the repo

--- ai/test_ai.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-from ai_player import AIPlayer
-
-class TestAI(AIPlayer):
-    # Takes in a 2d array indicating the state of the game
-    # returns an int that indicates which move they would like to make
-    def make_move(self, state):
-        print "TeST"
-        return randint(0, 3)
